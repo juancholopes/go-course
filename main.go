091f27c01ca5509deba028a1c9643ce59621b752
juancholopes <177896874+juancholopes@users.noreply.github.com>
2026-02-09 10:42:02 -0500

    feat: agregar archivo types-variables.go con constantes y variables

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+)
+
+const FLOAT = MULTIPLICADOR
+
 func main() {
 	print("Hello from a module")
+	fmt.Println( "\n", y, "\n", entero, "\n", otroNumero,"\n", calcular(), "\n", string, FLOAT)
 } 
---
+++ types-variables.go
@@ -0,0 +1,24 @@
+package main
+
+const y = 3 // son constantes No inmutables es decir su valor no cambia en el tiempo y solo recibe strings booleanos y enteros
+
+var entero int // por defecto es nil que es el equivalente a null 
+var otroNumero int = 0 // también se puede asiganar el valor de una vez 
+
+// Cuando inicializamos la varible con un valor 
+var string = "hola" // Infiere también el tipo 
+var anotherNumber = 2 // Infiere el tipo 
+var pointer = anotherNumber
+
+// var iniciarlizarSinTipo := "hola" // Solo funciona dentro de funciones
+
+
+const MULTIPLICADOR = 100000 // Go lo que en escencia hace en tiempo de compilación es copiar y pegar ese valor en el lugar donde se llame la variable
+
+
+func calcular() int {
+	numero := 10 // Infiere el tipo pero solo se usa dentro del scope de funciones no el scope global 
+	return numero * MULTIPLICADOR / anotherNumber * pointer // Aquí en realidad Go remplaza por el valor 10000 o el valor que tenga asignado 
+}
+
+
